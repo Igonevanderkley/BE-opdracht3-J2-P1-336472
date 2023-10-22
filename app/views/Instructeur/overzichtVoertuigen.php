c61f2a91a12b26941f5e5db043dd2e607d48fd42
Igone van_der_Kley <igone@vdkley.nl>
2023-10-22 18:54:55 +0200

    overzichtVoertuigen view added delete message

--- app/views/Instructeur/overzichtVoertuigen.php
+++ app/views/Instructeur/overzichtVoertuigen.php
@@ -1,16 +1,19 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Ongebruikte voertuigen</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
     <link rel="stylesheet" href="<?= URLROOT; ?>/css/style.css">
 </head>
-  <body>
-    
+
+<body>
+
     <h3><u><?= $data['title']; ?></u></h3>
+
 
     <table>
         <tbody>
@@ -26,11 +29,13 @@
                 <th>Aantal Sterren</th>
                 <td><?= $data['aantalSterren']; ?></td>
             </tr>
-        </tbody
-        >
+        </tbody>
     </table>
-    
+
     <?= $data['toevoegen']; ?>
+
+
+
 
     <table>
         <thead>
@@ -42,16 +47,18 @@
             <th>Brandstof</th>
             <th>RijbewijsCategorie</th>
             <th>Wijzig</th>
+            <th>Verwijderen</th>
         </thead>
         <tbody>
             <?= $data['tableRows']; ?>
         </tbody>
     </table>
 
-
-
-
+    <?php if (isset($data['deleteMessage'])) : ?>
+        <div><?= $data['deleteMessage']; ?></div>
+    <?php endif ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-  </body>
+</body>
+
 </html>
