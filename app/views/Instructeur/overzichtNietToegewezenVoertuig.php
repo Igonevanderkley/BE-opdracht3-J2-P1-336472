d869cd84ea74f1b2ce009e07fbf381babe135057
Igone van_der_Kley <igone@vdkley.nl>
2023-10-22 18:53:56 +0200

    overzichtNietToegewezenVoertuig view added delete message

--- app/views/Instructeur/overzichtNietToegewezenVoertuig.php
+++ app/views/Instructeur/overzichtNietToegewezenVoertuig.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Niet toegenwezen voertuigen</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
     <link rel="stylesheet" href="<?= URLROOT; ?>/css/style.css">
 </head>
+
 <body>
-    
+
     <h3><u><?= $data['title']; ?></u></h3>
 
     <table>
@@ -26,10 +28,9 @@
                 <th>Aantal Sterren</th>
                 <td><?= $data['aantalSterren']; ?></td>
             </tr>
-        </tbody
-        >
+        </tbody>
     </table>
-    
+
 
     <table>
         <thead>
@@ -39,18 +40,19 @@
             <th>Bouwjaar</th>
             <th>Brandstof</th>
             <th>RijbewijsCategorie</th>
-            <th>Toevoegen</th>
             <th>Wijzigen</th>
+            <th>Verwijderen</th>
         </thead>
         <tbody>
             <?= $data['tableRows']; ?>
         </tbody>
     </table>
 
-
-
-
+    <?php if (isset($data['deleteMessage'])) : ?>
+        <div><?= $data['deleteMessage']; ?></div>
+    <?php endif ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-  </body>
+</body>
+
 </html>
